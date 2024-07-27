fce97f366b996e9c7d9104dcdee1d67d09f2c8c3
Deepak <deepakworkpc@gmail.com>
2024-07-27 21:34:47 +0530

    .;

--- quickdemo.js
+++ quickdemo.js
@@ -1,7 +1,14 @@
-let http = require("http")
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-    console.log("Request inbound")
-  }).listen(8080);
+const http = require("http");
+const fs = require("fs");
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('<h1>Hello World</h1>');
+//     console.log("Request inbound")
+//   }).listen(8080);
   
+fs.rename('README.mdx', 'README.md', err => {
+    if (err) {
+        return console.error(err)
+    }
+})
