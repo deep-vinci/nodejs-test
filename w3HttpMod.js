b7516fa66f576295cffce002922936f03ea6c1b1
Deepak <deepakworkpc@gmail.com>
2024-07-28 12:24:37 +0530

    url methods in js

--- w3HttpMod.js
+++ w3HttpMod.js
@@ -6,7 +6,8 @@ https.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     })
-    res.end(req.url)
-    // res.write("<h1>Hello world</h1>");  // write a response
-    // res.end();                          // end the response
+    // let q = url.parse(req.url, true).query;
+    // console.log(q.)
+    res.end(JSON.stringify(url.parse(req.url, true).query))
+
 }).listen(8080);                        // listens on 8080 port
