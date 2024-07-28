f8ecb3f0081e68a80a5ff8df41fe7cd66efad1d1
Deepak <deepakworkpc@gmail.com>
2024-07-28 15:23:45 +0530

    .

--- w3FsMod.js
+++ w3FsMod.js
@@ -1,11 +1,16 @@
 let fs = require("fs");
 let http = require("http");
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/html"
-    })
-    fs.readFile("index.html", "utf-8", (err, data) => {
-        res.end(data)
-    })
-}).listen(8000)
+// http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         "Content-Type": "text/html"
+//     })
+//     fs.readFile("index.html", "utf-8", (err, data) => {
+//         res.end(data)
+//     })
+// }).listen(8000)
+
+fs.unlink("index.html2", err => {
+    if (err) throw err;
+    console.log("Deleted!")
+})
