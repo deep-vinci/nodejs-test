9a2a20d7784446d4c6103e8d587905cf6c28f45f
Deepak <deepakworkpc@gmail.com>
2024-07-28 12:16:57 +0530

    url methods

--- w3HttpMod.js
+++ w3HttpMod.js
@@ -1,11 +1,12 @@
 let https = require("http");
+let url = require("url");
 
 https.createServer((req, res) => {
     
     res.writeHead(200, {
         'Content-Type': 'text/html'
     })
-    
-    res.write("<h1>Hello world</h1>");  // write a response
-    res.end();                          // end the response
+    res.end(req.url)
+    // res.write("<h1>Hello world</h1>");  // write a response
+    // res.end();                          // end the response
 }).listen(8080);                        // listens on 8080 port
